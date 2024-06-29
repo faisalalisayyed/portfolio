35a56399c23d76dc254a39bb81abccf39b9e9ab8
c0de-slayer <fsali315@gmail.com>
2024-06-29 15:39:28 +0530

    Add vercel speed insight

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import styled, { ThemeProvider } from 'styled-components';
 import { Loader, Nav, Social, Email, Footer } from '@components';
 import { GlobalStyle, theme } from '@styles';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const StyledContent = styled.div`
   display: flex;
